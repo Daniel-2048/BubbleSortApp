eebfbf15acfc73cfce5d59590d24d7d5661c43a7
Daniel-2048 <85775654+Daniel-2048@users.noreply.github.com>
2024-11-01 21:37:45 +0800

    Rewrite the logic of the bubble sort

--- BubbleSortApp/bubbleSort.swift
+++ BubbleSortApp/bubbleSort.swift
@@ -10,31 +10,20 @@
 
 //use [_] to ignore the label of the parameters, so can use bubbleSort(&array) directly.
 
-var reverse: Bool = false
-
-
-func bubbleSort(_ array: inout [Float]) {
-    if reverse {
-        let number = array.count
-        
-        for i in 0..<number {
-            for j in 0..<number-i-1 {
-                if array[j] > array[j+1] {
-                    array.swapAt(j, j+1)
-                }
+//use reverse to adjust the condition
+func bubbleSort(_ array: inout [Float], reverse: Bool = false) {
+    let number = array.count
+    
+    for i in 0..<number {
+        for j in 0..<number-i-1 {
+            //All above things are of the same
+            //Only thing changing is the condition [if] parts -> A variable based on the [reverse]
+            let shouldSwap: Bool = reverse ? array[j] < array[j+1] : array[j] > array[j+1]
+            
+            if shouldSwap {
+                array.swapAt(j, j+1)
             }
-            print(array)
         }
-    } else if reverse == false {
-        let number = array.count
-        
-        for i in 0..<number {
-            for j in 0..<number-i-1 {
-                if array[j] < array[j+1] {
-                    array.swapAt(j, j+1)
-                }
-            }
-            print(array)
-        }
+        print(array)
     }
 }
