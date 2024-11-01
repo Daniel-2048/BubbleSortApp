71d9fc804b95b0ee25256912ea3b06075b407c34
Daniel-2048 <85775654+Daniel-2048@users.noreply.github.com>
2024-11-01 19:04:41 +0800

    Initial commit with start codes

---
+++ BubbleSortApp/BubbleSortAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubbleSortAppApp.swift
+//  BubbleSortApp
+//
+//  Created by Daniel Zhang on 01/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct BubbleSortAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BubbleSortAppTests/BubbleSortAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BubbleSortAppTests.swift
+//  BubbleSortAppTests
+//
+//  Created by Daniel Zhang on 01/11/2024.
+//
+
+import Testing
+@testable import BubbleSortApp
+
+struct BubbleSortAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
